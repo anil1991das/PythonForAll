082f9fe4aeeec81564204cdb281f49c1d4407398
ANIL KUMAR DAS <39938998+anil1991das@users.noreply.github.com>
2020-06-04 22:03:10 +0530

    Python basics

---
+++ Comments.py
@@ -0,0 +1,22 @@
+#This my Python program which described comment
+# Single line Comments
+
+"""Approach 1   Multi line comments
+This my Python program which described comment
+This my Python program which described comment
+This my Python program which described comment
+This my Python program which described comment
+This my Python program which described comment"""
+
+
+'''Approach 1
+This my Python program which described comment
+This my Python program which described comment
+This my Python program which described comment
+This my Python program which described comment
+This my Python program which described comment'''
+
+
+
+
+print("i am working with python")
---
+++ RedeclarationVariables.py
@@ -0,0 +1,9 @@
+a=10
+print(a)
+
+a=100
+print(a)   #re-declaration
+
+a="welcome"
+print(a)
+
